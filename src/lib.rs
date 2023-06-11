f09da2f214de62e956af2c43ed301ad9ab9818b7
AsPulse <aspulse.x@gmail.com>
2023-06-11 22:04:55 +0900

    Refactor error message for missing paths.yml file

--- src/lib.rs
+++ src/lib.rs
@@ -8,18 +8,19 @@ pub fn run() {
   let _config = Config::new().unwrap_or_else(|err| {
     match err {
       LoadConfigError::NoPathsConfigFileFound { config_path } => {
-        let mut config_path = config_path;
-        config_path.push("paths.yml");
+        let mut config_yaml_path = config_path.clone();
+        config_yaml_path.push("paths.yml");
         eprintln!(
-          "{} {}\nRun this to edit: {}",
+          " {} {}\n\n Run following commands to edit:\n {}\n {}",
           " ✖ERROR ".on_red().white().bold(),
           "paths.yml not found in your config directory.".red(),
-          format!("$ vim \"{}\"", config_path.to_str().unwrap()).bold(),
+          format!("$ mkdir -p \"{}\"", config_path.to_str().unwrap()).bold(),
+          format!("$ vim \"{}\"", config_yaml_path.to_str().unwrap()).bold(),
         );
       }
       LoadConfigError::PathConfigInvalidYamlFormat { paths_yml_path } => {
         eprintln!(
-          "{} {}\nPlease check the file has 'repos' properties with valid yaml format.",
+          " {} {}\n Please check the file has 'repos' properties with valid yaml format.",
           " ✖ERROR ".on_red().white().bold(),
           format!("your config file, {} is invalid.", paths_yml_path.bold()).red(),
         );
