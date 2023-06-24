c55f65782d5f009345c77a850b139056b7e90ba6
AsPulse <84216737+AsPulse@users.noreply.github.com>
2023-06-24 12:51:44 +0900

    [fix] paths.yml to config.yml (#7)
    * Refactor config.rs with new struct name and file name
    
    * Run cargo fmt
    
    * Rename paths.yml to config.yml for all config files
    
    * Fix config file paths in tests

--- src/config.rs
+++ src/config.rs
@@ -4,19 +4,19 @@ use thiserror::Error;
 use yaml_rust::YamlLoader;
 
 #[derive(Debug)]
-pub struct Config {
-  pub config_path: PathBuf,
+pub struct LocalStorage {
+  pub root_path: PathBuf,
   pub repo_paths: Vec<PathBuf>,
 }
 
-impl Config {
+impl LocalStorage {
   pub fn new() -> Result<Self, LoadConfigError> {
     let mut dir = config_local_dir().unwrap();
     dir.push("popi");
-    Self::new_from_config_path(dir)
+    Self::new_from_root_path(dir)
   }
-  pub fn new_from_config_path(config_path: PathBuf) -> Result<Self, LoadConfigError> {
-    load_config(config_path)
+  pub fn new_from_root_path(root_path: PathBuf) -> Result<Self, LoadConfigError> {
+    load_localstorage(root_path)
   }
 }
 
@@ -43,49 +43,49 @@ fn read_file_with_priority(
 
 #[derive(Error, Debug, PartialEq, Eq)]
 pub enum LoadConfigError {
-  #[error("Could not find any paths config file")]
-  NoPathsConfigFileFound { config_path: PathBuf },
-  #[error("Paths config has invalid yaml format")]
-  PathConfigInvalidYamlFormat { paths_yml_path: String },
+  #[error("Could not find any paths config.yml")]
+  NoConfigFileFound { root_path: PathBuf },
+  #[error("config.yml has invalid yaml format")]
+  ConfigInvalidYamlFormat { config_yml_path: String },
 }
 
-fn load_config(config_path: PathBuf) -> Result<Config, LoadConfigError> {
-  let (paths_yml, paths_yml_path) =
-    read_file_with_priority(&config_path, vec!["paths.yml", "paths.yaml"]).map_err(|_| {
-      LoadConfigError::NoPathsConfigFileFound {
-        config_path: config_path.to_path_buf(),
+fn load_localstorage(root_path: PathBuf) -> Result<LocalStorage, LoadConfigError> {
+  let (config_yml, config_yml_path) =
+    read_file_with_priority(&root_path, vec!["config.yml", "config.yaml"]).map_err(|_| {
+      LoadConfigError::NoConfigFileFound {
+        root_path: root_path.to_path_buf(),
       }
     })?;
 
-  let paths_payload = YamlLoader::load_from_str(&paths_yml).map_err(|_| {
-    LoadConfigError::PathConfigInvalidYamlFormat {
-      paths_yml_path: paths_yml_path.to_string(),
+  let config_payload = YamlLoader::load_from_str(&config_yml).map_err(|_| {
+    LoadConfigError::ConfigInvalidYamlFormat {
+      config_yml_path: config_yml_path.to_string(),
     }
   })?;
 
-  let repos = &paths_payload
+  let repos = &config_payload
     .get(0)
-    .ok_or(LoadConfigError::PathConfigInvalidYamlFormat {
-      paths_yml_path: paths_yml_path.to_string(),
+    .ok_or(LoadConfigError::ConfigInvalidYamlFormat {
+      config_yml_path: config_yml_path.to_string(),
     })?["repos"]
     .as_vec()
-    .ok_or(LoadConfigError::PathConfigInvalidYamlFormat {
-      paths_yml_path: paths_yml_path.to_string(),
+    .ok_or(LoadConfigError::ConfigInvalidYamlFormat {
+      config_yml_path: config_yml_path.to_string(),
     })?;
   let repo_paths = &repos
     .iter()
     .map(|repo| {
       let repo_path = repo
         .as_str()
-        .ok_or(LoadConfigError::PathConfigInvalidYamlFormat {
-          paths_yml_path: paths_yml_path.to_string(),
+        .ok_or(LoadConfigError::ConfigInvalidYamlFormat {
+          config_yml_path: config_yml_path.to_string(),
         })?;
       Ok(PathBuf::from(repo_path))
     })
     .collect::<Result<Vec<PathBuf>, LoadConfigError>>()?;
 
-  Ok(Config {
-    config_path,
+  Ok(LocalStorage {
+    root_path,
     repo_paths: repo_paths.clone(),
   })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,28 @@
 pub mod config;
 use colored::Colorize;
-use config::{Config, LoadConfigError};
+use config::{LoadConfigError, LocalStorage};
 
 pub fn run() {
   startup_message();
 
-  let _config = Config::new().unwrap_or_else(|err| {
+  let _config = LocalStorage::new().unwrap_or_else(|err| {
     match err {
-      LoadConfigError::NoPathsConfigFileFound { config_path } => {
-        let mut config_yaml_path = config_path.clone();
-        config_yaml_path.push("paths.yml");
+      LoadConfigError::NoConfigFileFound { root_path } => {
+        let mut config_yaml_path = root_path.clone();
+        config_yaml_path.push("config.yml");
         eprintln!(
           " {} {}\n\n Run following commands to edit:\n {}\n {}",
           " ✖ERROR ".on_red().white().bold(),
-          "paths.yml not found in your config directory.".red(),
-          format!("$ mkdir -p \"{}\"", config_path.to_str().unwrap()).bold(),
+          "config.yml not found in your config directory.".red(),
+          format!("$ mkdir -p \"{}\"", root_path.to_str().unwrap()).bold(),
           format!("$ vim \"{}\"", config_yaml_path.to_str().unwrap()).bold(),
         );
       }
-      LoadConfigError::PathConfigInvalidYamlFormat { paths_yml_path } => {
+      LoadConfigError::ConfigInvalidYamlFormat { config_yml_path } => {
         eprintln!(
           " {} {}\n Please check the file has 'repos' properties with valid yaml format.",
           " ✖ERROR ".on_red().white().bold(),
-          format!("your config file, {} is invalid.", paths_yml_path.bold()).red(),
+          format!("your config file, {} is invalid.", config_yml_path.bold()).red(),
         );
       }
     }
