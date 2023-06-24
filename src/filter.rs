8ccd3d660aa511078e3ddd49e4b5b584d33aca9c
AsPulse <aspulse.x@gmail.com>
2023-06-24 20:27:01 +0900

    Add stringmetrics dependency and fuzzy_match method to PopiFilter

--- src/filter.rs
+++ src/filter.rs
@@ -2,9 +2,9 @@ pub struct PopiFilter {}
 
 #[derive(Debug, PartialEq)]
 pub struct MatchedString {
-  pub matched_start: u32,
-  pub matched_length: u32,
-  pub distance: u32,
+  pub matched_start: usize,
+  pub matched_length: usize,
+  pub distance: usize,
 }
 
 #[derive(Debug, PartialEq)]
@@ -15,12 +15,67 @@ pub enum MatchedResult {
 
 impl PopiFilter {
   pub fn fuzzy_match(keyword: &str, target: &str) -> MatchedResult {
-    MatchedResult::NotMatched()
+    let keyword_len = keyword.chars().count();
+
+    let mut keyword_filter: (usize, usize) = (0, 0);
+    loop {
+      if keyword_filter.0 + keyword_filter.1 >= keyword_len {
+        return MatchedResult::NotMatched();
+      }
+      let clipped_keyword = &keyword[
+        keyword.char_indices().nth(keyword_filter.0).unwrap().0
+        ..
+        keyword.char_indices().nth(keyword_len - keyword_filter.1).map_or(keyword_len, |v| v.0)
+      ];
+
+      let start = target.char_indices().find(|c| c.1 == clipped_keyword.chars().next().unwrap()).map(|v| v.0);
+      let end = target.char_indices().rev().find(|c| c.1 == clipped_keyword.chars().last().unwrap()).map(|v| v.0);
+      if start.is_some() && end.is_some() && start.unwrap() <= end.unwrap() {
+        let start = start.unwrap();
+        let end = end.unwrap();
+        let clipped_target = &target[start..end + 1];
+        return MatchedResult::Matched(MatchedString {
+          matched_start: start,
+          matched_length: end - start + 1,
+          distance: usize::try_from(stringmetrics::levenshtein(clipped_target, clipped_keyword)).unwrap() + keyword_filter.0 + keyword_filter.1,
+        });
+      }
+      keyword_filter = PopiFilter::next_start_and_end(keyword_filter);
+    }
+  }
+
+  pub(super) fn next_start_and_end(current: (usize, usize)) -> (usize, usize) {
+    let (start, end) = current;
+    if start == 0 {
+      (end + 1, 0)
+    } else {
+      (start - 1, end + 1)
+    }
+  }
+
+}
+
+#[cfg(test)]
+mod test_next_start_and_end {
+  use super::*;
+
+  #[test]
+  fn concurrent() {
+    assert_eq!(PopiFilter::next_start_and_end((0, 0)), (1, 0));
+    assert_eq!(PopiFilter::next_start_and_end((1, 0)), (0, 1));
+    assert_eq!(PopiFilter::next_start_and_end((0, 1)), (2, 0));
+    assert_eq!(PopiFilter::next_start_and_end((2, 0)), (1, 1));
+    assert_eq!(PopiFilter::next_start_and_end((1, 1)), (0, 2));
+    assert_eq!(PopiFilter::next_start_and_end((0, 2)), (3, 0));
+    assert_eq!(PopiFilter::next_start_and_end((3, 0)), (2, 1));
+    assert_eq!(PopiFilter::next_start_and_end((2, 1)), (1, 2));
+    assert_eq!(PopiFilter::next_start_and_end((1, 2)), (0, 3));
+    assert_eq!(PopiFilter::next_start_and_end((0, 3)), (4, 0));
   }
 }
 
 #[cfg(test)]
-mod tests {
+mod test_fuzzy_match {
   use super::*;
 
   #[test]
@@ -98,6 +153,34 @@ mod tests {
       MatchedResult::Matched(MatchedString {
         matched_start: 0,
         matched_length: 5,
+        distance: 1,
+      })
+    );
+    assert_eq!(
+      PopiFilter::fuzzy_match("bdf", "abcdefg"),
+      MatchedResult::Matched(MatchedString {
+        matched_start: 1,
+        matched_length: 5,
+        distance: 2,
+      })
+    );
+    assert_eq!(
+      PopiFilter::fuzzy_match("abcd", "abc"),
+      MatchedResult::Matched(MatchedString {
+        matched_start: 0,
+        matched_length: 3,
+        distance: 1,
+      })
+    );
+  }
+
+  #[test]
+  fn matched_with_distance_and_duplicates() {
+    assert_eq!(
+      PopiFilter::fuzzy_match("atisiie", "satisfied"),
+      MatchedResult::Matched(MatchedString {
+        matched_start: 1,
+        matched_length: 7,
         distance: 1,
       })
     );
